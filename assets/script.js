7611fe75025b2567ab04acf7a6f85465c883f393
Vlada <111080921+VladaBolotova@users.noreply.github.com>
2022-11-01 21:54:07 -0400

    changing HTML

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,119 @@
-var APIKey = "090c01416bd9fb914720dfa9cb80f458";
+const APIKey = "090c01416bd9fb914720dfa9cb80f458";
 
 var city;
 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+
+
+
+
 fetch(queryURL)
 
 
+
+
+
+// var userFormEl = document.querySelector('#user-form');
+// var cityButtonsEl = document.querySelector('#city-buttons');
+// var nameInputEl = document.querySelector('#username');
+// var cityContainerEl = document.querySelector('#city-container');
+// var citySearchTerm = document.querySelector('#city-term');
+
+// var formSubmitHandler = function (event) {
+//   event.preventDefault();
+
+//   var username = nameInputEl.value.trim();
+
+//   if (username) {
+//     getUserRepos(username);
+
+//     cityContainerEl.textContent = '';
+//     nameInputEl.value = '';
+//   } else {
+//     alert('Please enter a GitHub username');
+//   }
+// };
+
+// var buttonClickHandler = function (event) {
+//   var language = event.target.getAttribute('data-city');
+
+//   if (city) {
+//     getFeaturedRepos(city);
+
+//     cityContainerEl.textContent = '';
+//   }
+// };
+
+// var getUserRepos = function (user) {
+//   var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+
+//   fetch(apiUrl)
+//     .then(function (response) {
+//       if (response.ok) {
+//         response.json().then(function (data) {
+//           displayRepos(data, user);
+//         });
+//       } else {
+//         alert('Error: ' + response.statusText);
+//       }
+//     })
+//     .catch(function (error) {
+//       alert('Unable to connect to GitHub');
+//     });
+// };
+
+// var getFeaturedRepos = function (city) {
+//   var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+//   fetch(apiUrl).then(function (response) {
+//     if (response.ok) {
+//       response.json().then(function (data) {
+//         displayRepos(data.items, language);
+//       });
+//     } else {
+//       alert('Error: ' + response.statusText);
+//     }
+//   });
+// };
+
+// var displayRepos = function (city, cityTerm) {
+//   if (repos.length === 0) {
+//     cityContainerEl.textContent = 'No repositories found.';
+//     return;
+//   }
+
+//   citySearchTerm.textContent = cityTerm;
+
+//   for (var i = 0; i < city.length; i++) {
+//     var repoName = city[i].owner.login + '/' + city[i].name;
+
+//     var repoEl = document.createElement('div');
+//     repoEl.classList = 'list-item flex-row justify-space-between align-center';
+
+//     var titleEl = document.createElement('span');
+//     titleEl.textContent = repoName;
+
+//     repoEl.appendChild(titleEl);
+
+//     var statusEl = document.createElement('span');
+//     statusEl.classList = 'flex-row align-center';
+
+//     if (city[i].open_issues_count > 0) {
+//       statusEl.innerHTML =
+//         "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+//     } else {
+//       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+//     }
+
+//     repoEl.appendChild(statusEl);
+
+//     cityContainerEl.appendChild(repoEl);
+//   }
+// };
+
+// userFormEl.addEventListener('submit', formSubmitHandler);
+// cityButtonsEl.addEventListener('click', buttonClickHandler);
 
 
 
