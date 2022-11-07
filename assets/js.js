43e0ab80e7752130ff845eecd13b1ec1e9942021
Vlada <111080921+VladaBolotova@users.noreply.github.com>
2022-11-07 18:15:59 -0500

    scriptjs

--- assets/js.js
+++ assets/js.js
@@ -1,50 +1,32 @@
-// const APIKey = "090c01416bd9fb914720dfa9cb80f458";
 
-// var city;
-
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-
+   var weathericon = response.weather[0].icon;
+   var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+   var date=new Date(response.dt*1000).toLocaleDateString();
+   $("#current-city").html(response.name +"("+date+")" +"<img src="+iconurl+">");
 
 
 
-// fetch(queryURL)
 
-function GetInfo(){
-    const cityName = document.getElementById("search-city");
-    const cityList = document.getElementById("city-list");
-    cityList.innerHTML = "--"+cityName.value+"--"
+//add a list//
+
+var cityInput = document.getElementById("city-name");
+var cityList = document.getElementById("city-list");
 
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?q=" +cityName.value+ "&appid=223570223a859abd2a47ee0f27ed73e1")
-.then(response => response.json())
-.then(data =>{
-    for (i=0;i<5;i++){
-        document.getElementById("current-city" +(i+1)+"temperature").innerHTML = "temperature:" + Number(data.list[i].main.temp -302.79).toFixed(1)+"°";
-    }
-    for (i=0;i<5;i++){
-        document.getElementById("current-city" +(i+1)+"humidity").innerHTML = "humidity:" + Number(data.list[i].main.humidity +"%");
-    }
-    for (i=0;i<5;i++){
-        document.getElementById("current-city" +(i+1)+"wind-speed").innerHTML = "wind-speed:" + Number(data.list[i].wind.speed *5.14).toFixed(1);
-    }
-})
+function handleFormSearch(event) {
+    event.preventDefault();
 
-.catch(err => alert("Something went wrong"))
+    var list = $('input[name="search-city"]').val();
+
+if(!list) {
+    console.log('No city filled out in form');
+    return;
 }
 
-const w= new Date();
-const weekday =["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+cityList.append('<li>' + list + '<li>');
 
-function weekDay(day){
-    if(day +w.getDay() > 6){
-        return day +w.getDay()-7;
-    }
-    else{
-        return day +w.getDay();
-    }
-    }
+$('input[name="search-city"]').val('');
 
-for(i=o;i<5;i++){
-    document.getElementById(" " +(i+1)).innerHTML = weekday[weekDay];
 }
+
+cityInput.onabort('submit', handleFormSearch);
--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,9 @@ var APIKey="223570223a859abd2a47ee0f27ed73e1";
  var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
    
 var city=document.getElementById("search-city");
-var button=document.getElementById("Search")
+var button=document.getElementById("Search");
+var currentCity=document.getElementById("current-city");
+
 
 
 
@@ -28,19 +30,30 @@ button.addEventListener("click",function(){
  
 
  
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city.value}&limit=5&appid=${APIKey}`).then(function(response){
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city.value}&limit=1&appid=${APIKey}`).then(function(response){
     return response.json();
-}).then(function(data){
-    var lab=data[0].lat
-    var lon=data[0].lon
+}).then(function(cityInfo){
+    var lab=cityInfo[0].lat
+    
+    var lon=cityInfo[0].lon
+console.log(cityInfo);
+
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lab}&lon=${lon}&appid=${APIKey}`).then(function(response){
    return response.json();
-}).then(function(data){
-   var fiveDaysForecast=get5DaysForecast(data);
+} ).then(function(data){ 
+    console.log("data", data);
+var fiveDaysForecast=get5DaysForecast(data);
+  var weathericon = fiveDaysForecast[0].weather[0].icon;
+    var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+    var date=new Date(fiveDaysForecast[0].dt*1000).toLocaleDateString();
+    $("#current-city").html(cityInfo[0].name +"("+date+")" +"<img src="+iconurl+">");
+    
+   
    console.log(fiveDaysForecast);
    var dataTemp = fiveDaysForecast[0].main.temp
    var currentTemp = document.getElementById('temperature')
    currentTemp.textContent = ' ' + Math.round(dataTemp -273.15) + 'C'
+
 
    var dataHumidity = fiveDaysForecast[0].main.humidity
    var currentHumidity = document.getElementById('humidity')
@@ -51,11 +64,28 @@ fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city.value}&limit=5&appi
    var currentWind = document.getElementById('wind-speed')
    currentWind.textContent = ' ' +dataWind 
 
+//    var data1Humidity = fiveDaysForecast[1].main.humidity
+//    var firstDayHumidity = document.getElementById('fHumidity0')
+//    firstDayHumidity.textContent = ' ' +data1Humidity + '%'
+
+      
+for (i=0;i<5;i++){
+    var date= new Date((data.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+    var iconcode= data.list[((i+1)*8)-1].weather[0].icon;
+    var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+    var tempK= data.list[((i+1)*8)-1].main.temp;
+    var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+    var humidity= data.list[((i+1)*8)-1].main.humidity;
+    var windS = data.list[((i+1)*8)-1].wind.speed;
+
+    $("#fDate"+i).html(date);
+    $("#fImg"+i).html("<img src="+iconurl+">");
+    $("#fTemp"+i).html(tempF+"&#8457");
+    $("#fHumidity"+i).html(humidity+"%");
+    $("#fWindSpeed"+i).html(windS);
+
+}
 
 });
 })
 })
-//Example usage
-/*
-When the button is clicked,
-Then we get the user's input from the input tag and then call the api*/
