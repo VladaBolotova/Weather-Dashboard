2dde654b96b17ace96c12fbc9b212e8031d448a2
Vlada <111080921+VladaBolotova@users.noreply.github.com>
2022-11-07 10:58:45 -0500

    java script change

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,50 @@
-// const APIKey = "090c01416bd9fb914720dfa9cb80f458";
 
-// var city;
+var APIKey="223570223a859abd2a47ee0f27ed73e1";
 
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+ var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+   
+var city=document.getElementById("search-city");
+var button=document.getElementById("Search")
 
 
 
 
 
-// fetch(queryURL)
-
-function GetInfo(){
-    const cityName = document.getElementById("search-city");
-    const cityList = document.getElementById("city-list");
-    cityList.innerHTML = "--"+cityName.value+"--"
+function get5DaysForecast(apiResponse) {
+    return apiResponse.list.reduce(function(days,current) {
+        var currentDate=current.dt_txt.split(" ")[0];
+        if(!days.find(function(day) {
+            return day.dt_txt.includes(currentDate);
+        })) {
+            return [...days,current];
+        }
+        return days;
+    },[])
 }
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?q=" +cityName.value+ "&appid=223570223a859abd2a47ee0f27ed73e1")
-.then(response => response.json())
-.then(data =>{
-    for (i=0;i<5;i++){
-        document.getElementById("current-city" +(i+1)+"temperature").innerHTML = "temperature:" + Number(data.list[i].main.temp -302.79).toFixed(1)+"°";
-    }
-    for (i=0;i<5;i++){
-        document.getElementById("current-city" +(i+1)+"humidity").innerHTML = "humidity:" + Number(data.list[i].main.humidity -73).toFixed(1)+"°";
-    }
-})
 
+button.addEventListener("click",function(){
+ console.log(city.value)
+ 
+
+ 
+fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city.value}&limit=5&appid=${APIKey}`).then(function(response){
+    return response.json();
+}).then(function(data){
+    var lab=data[0].lat
+    var lon=data[0].lon
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lab}&lon=${lon}&appid=${APIKey}`).then(function(response){
+   return response.json();
+}).then(function(data){
+   var fiveDaysForecast=get5DaysForecast(data);
+   console.log(fiveDaysForecast);
+   var dataTemp = fiveDaysForecast[0].main.temp
+   var currentTemp = document.getElementById('temperature')
+   currentTemp.textContent = ' ' + Math.round(dataTemp -273.15) + 'C'
+});
+})
+})
+//Example usage
+/*
+When the button is clicked,
+Then we get the user's input from the input tag and then call the api*/
